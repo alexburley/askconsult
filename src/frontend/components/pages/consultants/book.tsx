0a16c6e80febae1bc35d6a8d842d047069271c2e
Alex Burley <alexburley3@live.co.uk>
2022-12-12 17:48:31 +0000

    Start nav

--- src/frontend/components/pages/consultants/book.tsx
+++ src/frontend/components/pages/consultants/book.tsx
@@ -8,12 +8,53 @@ export interface BookConsultantProps {
   className?: string;
 }
 
+const WizardStepContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  background-color: ${(p) => p.theme.colors.primary.light};
+  width: fit-content;
+  margin: auto;
+  padding: 16px 32px;
+  gap: 16px;
+`;
+const WizardStep = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 64px;
+`;
+
+const WizardStepNumber = styled.div`
+  background-color: ${(p) => p.theme.colors.primary.dark};
+  border-radius: 50%;
+  text-align: center;
+  width: 40px;
+  font-size: 24px;
+  height: 40px;
+`;
+const WizardStepText = styled.span``;
+
 const BookConsultantPage = ({ className }: BookConsultantProps) => {
   const [consultant, setConsultant] = useState<ConsultantModel>();
   const router = RoutingHelper.useRouter();
   const { consultantId } = RoutingHelper.usePathParams(router);
 
-  return <PageWrapper className={className}></PageWrapper>;
+  return (
+    <PageWrapper className={className}>
+      <WizardStepContainer>
+        {[
+          { step: 1, name: "Query" },
+          { step: 2, name: "Pay" },
+          { step: 3, name: "Confirm" },
+        ].map(({ step, name }) => (
+          <WizardStep key={step}>
+            <WizardStepNumber>{step}</WizardStepNumber>
+            <WizardStepText>{name}</WizardStepText>
+          </WizardStep>
+        ))}
+      </WizardStepContainer>
+    </PageWrapper>
+  );
 };
 
 export default BookConsultantPage;
