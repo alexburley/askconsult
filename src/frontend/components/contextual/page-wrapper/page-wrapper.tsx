abd28986866535c4c55e1fd92ae0ea8fe98c0b0b
Alex Burley <alexburley3@live.co.uk>
2022-12-23 15:49:32 +0000

    Minor.

--- src/frontend/components/contextual/page-wrapper/page-wrapper.tsx
+++ src/frontend/components/contextual/page-wrapper/page-wrapper.tsx
@@ -1,5 +1,4 @@
 import { BasePropsWithChildren } from "frontend/types/types";
-import styled from "styled-components";
 
 import Footer from "../footer";
 import Nav from "../nav";
@@ -8,28 +7,17 @@ export interface PageWrapperProps extends BasePropsWithChildren {
   noPad?: boolean;
 }
 
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-  margin: 0;
-`;
-
-const Container = styled.div<{ noPad: boolean }>`
-  padding-bottom: ${(p) => (p.noPad ? "0px" : "16px")};
-`;
-
 const PageWrapper = ({
   className,
   children,
   noPad = false,
 }: PageWrapperProps) => {
   return (
-    <Wrapper className={className}>
+    <div className={`flex-col h-screen m-0 ${className} `}>
       <Nav />
-      <Container noPad={noPad}>{children}</Container>
+      <div className={`${noPad && "pb-4"}`}>{children}</div>
       <Footer />
-    </Wrapper>
+    </div>
   );
 };
 
